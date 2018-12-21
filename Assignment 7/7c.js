8ba29156c46ee90a875db932cac040df865e439f
Leo Yang <leoyang388@gmail.com>
2018-12-20 19:38:11 -0800

    Added and modified codes in Assignment 7

---
+++ Assignment 7/7c.js
@@ -0,0 +1,14 @@
+function addEgg(){
+    document.getElementById("egg1").hidden = false;
+    document.getElementById("egg2").hidden = false;
+    document.getElementById("egg3").hidden = false;
+    document.getElementById("egg4").hidden = false;
+    document.getElementById("egg5").hidden = false;
+    document.getElementById("egg6").hidden = false;
+    document.getElementById("egg7").hidden = false;
+    document.getElementById("egg8").hidden = false;
+    document.getElementById("egg9").hidden = false;
+    document.getElementById("egg10").hidden = false;
+    document.getElementById("egg11").hidden = false;
+    document.getElementById("egg12").hidden = false;
+}
