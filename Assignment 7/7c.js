c42a90f88ebed362b75900150ff151eb8877b12c
Leo Yang <leoyang388@gmail.com>
2018-12-20 20:51:40 -0800

    Modified assignment 7

--- Assignment 7/7c.js
+++ Assignment 7/7c.js
@@ -1,14 +1,5 @@
-function addEgg(){
-    document.getElementById("egg1").hidden = false;
-    document.getElementById("egg2").hidden = false;
-    document.getElementById("egg3").hidden = false;
-    document.getElementById("egg4").hidden = false;
-    document.getElementById("egg5").hidden = false;
-    document.getElementById("egg6").hidden = false;
-    document.getElementById("egg7").hidden = false;
-    document.getElementById("egg8").hidden = false;
-    document.getElementById("egg9").hidden = false;
-    document.getElementById("egg10").hidden = false;
-    document.getElementById("egg11").hidden = false;
-    document.getElementById("egg12").hidden = false;
+function addEgg() {
+    var egg = document.createElement("IMG");
+    egg.setAttribute("src", "pics/egg.jpg");
+    document.body.appendChild(egg);
 }
