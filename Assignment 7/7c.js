e7af1968fc65e80cdb57c3caa9334dd5f76feb38
Leo Yang <SD02008515@richmw34579bel.rsd38.ca>
2019-01-29 14:50:11 -0800

    modified assignment 7c

--- Assignment 7/7c.js
+++ Assignment 7/7c.js
@@ -1,56 +1,26 @@
-var clickCounts = 0;
-var cartonCounts = 0;
+var x = 0;
+var y = 0;
 function addEgg() {
-    if (cartonCounts == 2) {
-        return;
+    x += 1;
+    document.getElementById("eggs").innerHTML += "<img class = 'egg' src = 'pics/download-1.jpg' width = '25%'alt = 'egg'>";
+    if (x * 1 == 12) {
+        y += 1;
+        document.getElementById("eggs").innerHTML = "";
+        document.getElementById("carton").innerHTML += "<img class = 'carton' src = 'pics/aea32332b6ba5a98c5c811e8dfbdbd5f_medium.png' alt = 'carton'>";
+        x = 0;
     }
-    
-    var egg = createImg("pics/egg.jpg", "120", "150");
-    document.getElementById("eggPan").appendChild(egg);
+    if (y == 1) {
 
-    var eggCarton = document.createElement("IMG");
-    eggCarton.setAttribute("src", "pics/eggcarton.png")
-  
-    var winPicture = createImg("pics/trophy.jpg", "300", "300");
-    clickCounts += 1;
-    document.getElementById("eggCount").innerHTML = clickCounts;
+        document.getElementById("henHome").innerHTML = "<img class = ' trophy' onClick = 'reset()' src='pics/download.png' alt = 'trophy'>";
 
-    if (clickCounts == 12) {
-        eggToCarton(eggCarton);
-    }
-    if (cartonCounts == 2) {
-       win(winPicture);
     }
 }
-
-function createImg(src, width, height){
-    var img = document.createElement("IMG");
-    img.setAttribute("src", src)
-    img.setAttribute("width", width);
-    img.setAttribute("height", height);
-    return img;
-}
-
-function eggToCarton(eggCarton){
-    document.getElementById("eggPan").innerHTML = "";
-    clickCounts = 0;
-    document.getElementById("eggShelf").appendChild(eggCarton);
-    cartonCounts += 1;
-    document.getElementById("cartonCount").innerHTML = cartonCounts;
-}
-
-function win(winPicture){
-    document.getElementById("winDisplay").appendChild(winPicture);
-    document.getElementById("button").style.visibility = "visible"
-}
-
 function reset() {
-    document.getElementById("eggPan").innerHTML = "";
-    document.getElementById("eggShelf").innerHTML = "";
-    document.getElementById("winDisplay").innerHTML = "";
-    document.getElementById("button").style.visibility = "hidden"
-    document.getElementById("eggCount").innerHTML = 0;
-    document.getElementById("cartonCount").innerHTML = 0;
-    clickCounts = 0;
-    cartonCounts = 0;
+
+    x = 0;
+    y = 0;
+    document.getElementById("henHome").innerHTML = "<img id = 'hen' class='hen' onClick='addEgg()' src='pics/download.jpg'>";
+    document.getElementById("eggs").innerHTML = "";
+    document.getElementById("carton").innerHTML = "";
+   
 }
