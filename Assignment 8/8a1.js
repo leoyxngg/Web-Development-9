8520b0431c893b8a2d8b2c3b1357408af7aebd3c
Leo Yang <leoyang388@gmail.com>
2019-01-29 20:23:34 -0800

    Finished assignment 8a1

---
+++ Assignment 8/8a1.js
@@ -0,0 +1,25 @@
+var randomNum = Math.floor((Math.random() * 10) + 1);
+var guess = 0;
+var guessNum = 0;
+while(guess != randomNum){
+   guessNum++;
+   guess = prompt("Guess a number between 1 to 10");
+
+   if(guess > randomNum){
+       larger();
+   }
+
+   if(guess < randomNum){
+       smaller();
+   }
+}
+
+function larger(){
+    alert("Your guess is larger than the number.");
+}
+
+function smaller(){
+    alert("Your guess is smaller than the number.")
+}
+
+alert("Congradulations! You have guessed the correct number! It took you a total of " + guessNum + " tries.");
