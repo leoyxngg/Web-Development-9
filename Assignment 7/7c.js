d177c4a18163a9d3223b1364bcc9c4d3d0c5eb80
Leo Yang <leoyang388@gmail.com>
2019-01-29 19:24:55 -0800

    Perfected 7c

--- Assignment 7/7c.js
+++ Assignment 7/7c.js
@@ -2,16 +2,16 @@ var x = 0;
 var y = 0;
 function addEgg() {
     x += 1;
-    document.getElementById("eggs").innerHTML += "<img class = 'egg' src = 'pics/download-1.jpg' width = '25%'alt = 'egg'>";
+    document.getElementById("eggs").innerHTML += "<img class = 'egg' src = 'pics/egg.jpg' width = '25%'alt = 'egg'>";
     if (x * 1 == 12) {
         y += 1;
         document.getElementById("eggs").innerHTML = "";
-        document.getElementById("carton").innerHTML += "<img class = 'carton' src = 'pics/aea32332b6ba5a98c5c811e8dfbdbd5f_medium.png' alt = 'carton'>";
+        document.getElementById("carton").innerHTML += "<img class = 'carton' src = 'pics/eggcarton.png' alt = 'carton'>";
         x = 0;
     }
-    if (y == 1) {
+    if (y == 2) {
 
-        document.getElementById("henHome").innerHTML = "<img class = ' trophy' onClick = 'reset()' src='pics/download.png' alt = 'trophy'>";
+        document.getElementById("henHome").innerHTML = "<img class = ' trophy' onClick = 'reset()' src='pics/trophy.jpg' alt = 'trophy'>";
 
     }
 }
@@ -19,7 +19,7 @@ function reset() {
 
     x = 0;
     y = 0;
-    document.getElementById("henHome").innerHTML = "<img id = 'hen' class='hen' onClick='addEgg()' src='pics/download.jpg'>";
+    document.getElementById("henHome").innerHTML = "<img id = 'hen' class='hen' onClick='addEgg()' src='pics/hen.jpg'>";
     document.getElementById("eggs").innerHTML = "";
     document.getElementById("carton").innerHTML = "";
    
