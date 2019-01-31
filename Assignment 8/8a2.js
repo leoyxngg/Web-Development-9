62e7fa45fb8e86c7295d829771db4ad32e4ad159
Leo Yang <leoyang388@gmail.com>
2019-01-30 22:09:41 -0800

    Integrated sounds into assignment 8a2

--- Assignment 8/8a2.js
+++ Assignment 8/8a2.js
@@ -1,10 +1,14 @@
 var randomNum = Math.floor((Math.random() * 10) + 1);
 var guess = 0;
 var guessNum = 0;
+var largerSound = new sound("sounds/larger.mp3");
+var smallerSound = new sound("sounds/smaller.mp3");
+var winSound = new sound("sounds/win.mp3");
 
 function guessNumber() {
     guess = document.getElementById("guess").value;
     if (guess == randomNum) {
+        winSound.play();
         alert("Congradulations! You have guessed the correct number! It took you a total of " + guessNum + " tries.");
         return;
     }
@@ -16,14 +20,30 @@ function guessNumber() {
             smaller();
         }
         guessNum++;
-        
     }
 }
 
 function larger() {
+    largerSound.play();
     alert("Your guess is larger than the number.");
 }
 
 function smaller() {
+    smallerSound.play();
     alert("Your guess is smaller than the number.");
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
 }
