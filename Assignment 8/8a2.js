f223be244ddf9a63b6237baa880ba78cd787244a
Leo Yang <leoyang388@gmail.com>
2019-01-31 23:46:53 -0800

    Beautified with atom beautifier

--- Assignment 8/8a2.js
+++ Assignment 8/8a2.js
@@ -6,44 +6,43 @@ var smallerSound = new sound("sounds/smaller.mp3");
 var winSound = new sound("sounds/win.mp3");
 
 function guessNumber() {
-    guess = document.getElementById("guess").value;
-    if (guess == randomNum) {
-        winSound.play();
-        alert("Congradulations! You have guessed the correct number! It took you a total of " + guessNum + " tries.");
-        return;
+  guess = document.getElementById("guess").value;
+  if (guess == randomNum) {
+    winSound.play();
+    alert("Congradulations! You have guessed the correct number! It took you a total of " + guessNum + " tries.");
+    return;
+  } else {
+    if (guess > randomNum) {
+      larger();
     }
-    else {
-        if (guess > randomNum) {
-            larger();
-        }
-        if (guess < randomNum) {
-            smaller();
-        }
-        guessNum++;
+    if (guess < randomNum) {
+      smaller();
     }
+    guessNum++;
+  }
 }
 
 function larger() {
-    largerSound.play();
-    alert("Your guess is larger than the number.");
+  largerSound.play();
+  alert("Your guess is larger than the number.");
 }
 
 function smaller() {
-    smallerSound.play();
-    alert("Your guess is smaller than the number.");
+  smallerSound.play();
+  alert("Your guess is smaller than the number.");
 }
 
 function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function () {
-        this.sound.play();
-    }
-    this.stop = function () {
-        this.sound.pause();
-    }
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause();
+  }
 }
