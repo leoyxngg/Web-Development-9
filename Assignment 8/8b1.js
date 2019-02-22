ae346abfb7bb670eaa2beb0fc607e5940ded7431
Leo Yang <SD02008515@richmw34579bel.rsd38.ca>
2019-02-22 14:45:09 -0800

    Added pictures

---
+++ Assignment 8/8b1.js
@@ -0,0 +1,6 @@
+var myRange = document.getElementById('myRange')
+var output = document.getElementById('output')
+output.innerHTML = myRange.value;
+function updateOutput(){
+   output.innerHTML = myRange.value;
+}
