8e75c5cef0c89fc9c2081ccc781eba977861d44d
Leo Yang <leoyang388@gmail.com>
2019-04-07 23:52:15 -0700

    Added onmouseover and onmouseout elements onto the images

--- Assignment 9/9a1.js
+++ Assignment 9/9a1.js
@@ -2,10 +2,23 @@ function flipOver(index) {
     var name = (document.getElementById("fc" + index).innerHTML).trim();
     var vertxt = '<img src="pics/txt' + index + '.png">';
     var verimg = '<img src="pics/img' + index + '.png">';
-    document.getElementById("fc" + index).innerHTML = vertxt;  
-    if (name[name.length-9] == "m"){
-        document.getElementById("fc"+index).innerHTML = vertxt
-    }else{
-        document.getElementById("fc"+index).innerHTML = verimg 
-    }   
-} 
+    document.getElementById("fc" + index).innerHTML = vertxt;
+    if (name[name.length - 9] == "m") {
+        document.getElementById("fc" + index).innerHTML = vertxt
+    } else {
+        document.getElementById("fc" + index).innerHTML = verimg
+    }
+}
+
+var audio = document.getElementById('myAudio');
+var source = document.getElementById('audioSource');
+
+function playAudio(index) {
+    source.src = "sounds/sound" + index + ".mp3";
+    audio.load();
+    audio.play();
+}
+
+function stopAudio() {
+    audio.pause();
+}
