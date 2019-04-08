7932bea6fbe83046483f6f1ebd3596b242652996
Leo Yang <leoyang388@gmail.com>
2019-04-07 23:26:34 -0700

    Given the images abilities to flip back once flipped in assignment 9a1, also added the sounds for each one of the compounds

--- Assignment 9/9a1.js
+++ Assignment 9/9a1.js
@@ -1,16 +1,11 @@
 function flipOver(index) {
-    console.log(index);
     var name = (document.getElementById("fc" + index).innerHTML).trim();
-    console.log(name);
     var vertxt = '<img src="pics/txt' + index + '.png">';
     var verimg = '<img src="pics/img' + index + '.png">';
-    document.getElementById("fc" + index).innerHTML = vertxt;
-    var name = (document.getElementById("fc" + index).innerHTML).trim();
-    console.log(name);
-    console.log(name[name.length - 9]);
-    if (name[name.length - 9] == "i") {
-        document.getElementById("fc" + index).innerHTML = vertxt;
-    } else {
-        document.getElementById("fc" + index).innerHTML = verimg;
-    }
+    document.getElementById("fc" + index).innerHTML = vertxt;  
+    if (name[name.length-9] == "m"){
+        document.getElementById("fc"+index).innerHTML = vertxt
+    }else{
+        document.getElementById("fc"+index).innerHTML = verimg 
+    }   
 } 
