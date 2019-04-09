0661957acf45353d1cb9049815ccb8078ed9698e
Leo Yang <leoyang388@gmail.com>
2019-04-08 21:41:43 -0700

    Commented out the fuction that is unnessary.

--- Assignment 9/9a1.js
+++ Assignment 9/9a1.js
@@ -1,24 +1,24 @@
-function flipOver(index) {
-    var name = (document.getElementById("fc" + index).innerHTML).trim();
-    var vertxt = '<img src="pics/txt' + index + '.png">';
-    var verimg = '<img src="pics/img' + index + '.png">';
-    document.getElementById("fc" + index).innerHTML = vertxt;
-    if (name[name.length - 9] == "m") {
-        document.getElementById("fc" + index).innerHTML = vertxt;
-    } else {
-        document.getElementById("fc" + index).innerHTML = verimg;
-    }
-}
+// function flipOver(index) {
+//     var name = (document.getElementById("fc" + index).innerHTML).trim();
+//     var vertxt = '<img src="pics/txt' + index + '.png">';
+//     var verimg = '<img src="pics/img' + index + '.png">';
+//     document.getElementById("fc" + index).innerHTML = vertxt;
+//     if (name[name.length - 9] == "m") {
+//         document.getElementById("fc" + index).innerHTML = vertxt;
+//     } else {
+//         document.getElementById("fc" + index).innerHTML = verimg;
+//     }
+// }
 
 var audio = document.getElementById('myAudio');
 var source = document.getElementById('audioSource');
 
 function playAudio(index) {
-    source.src = "sounds/sound" + index + ".mp3";
-    audio.load();
-    audio.play();
+  source.src = "sounds/sound" + index + ".mp3";
+  audio.load();
+  audio.play();
 }
 
 function stopAudio() {
-    audio.pause();
+  audio.pause();
 }
