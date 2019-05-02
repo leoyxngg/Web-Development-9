9238493e45343a493fab89faa68eaaa4acaa3748
Leo Yang <leoyang388@gmail.com>
2019-05-01 21:52:24 -0700

    Made the box being able to generate into html, but not perfected

--- Assignment 10/10a2.js
+++ Assignment 10/10a2.js
@@ -1,4 +1,4 @@
-var t0, difference, redCircle, redTriangle, redRectangle, blueCircle, blueTriangle, blueRectangle;
+var t0, difference, boxedNums, numOfTerms, redCircle, redTriangle, redRectangle, blueCircle, blueTriangle, blueRectangle;
 redCircle = "<img src='imgs/negativeOne.png'>";
 redTriangle = "<img src='imgs/negativeTen.png'>";
 redRectangle = "<img src='imgs/negativeHundred.png'";
@@ -9,9 +9,8 @@ blueRectangle = "img src='imgs/hundred.png'";
 function genTn() {
     t0 = document.getElementById("t0").value;
     difference = document.getElementById("d").value;
+    numOfTerms = document.getElementById("tn").value;
     var tn;
-    var numOfTerms = 12;
-    console.log('start genTn');
     document.getElementById('buildButton').style.display = 'none';
     for (n = 0; n < numOfTerms; n++) {
         tn = t0 * 1 + difference * n;
@@ -22,8 +21,20 @@ function genTn() {
 
 function buildNextOne(n, tn) {
     console.log('t' + n + " = " + tn);
+    document.getElementById("boxArea").innerHTML = '<div class="col-sm-4 col-md-2"><span id="boxed" class="center">t<sub>' + n + '</sub><span class="tn">' + tn + '</span><span><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"></span></span></div>'
 }
 
 function showButton() {
     document.getElementById('buildButton').style.display = '';
 }
+
+{/* <div class="col-sm-4 col-md-2"><span id="boxed" class="center">t<sub>0</sub><span class="tn">6</span><span id="invisible">space</span><span><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"></span></span></div> */}
+
+{/* <div class="col-sm-4 col-md-2">
+    <span id="boxed" class="center">
+        t<sub>0</sub>
+        <span class="tn">6</span>
+        <span id="invisible">space</span>
+        <span><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"><img src="imgs/one.png"></span>
+    </span>
+</div> */}
