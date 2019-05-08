eff43f4a1f5057c1de283435a57adc79adc791eb
Leo Yang <leoyang388@gmail.com>
2019-05-07 21:36:17 -0700

    Added the feature of being able to display the value of the slidebars

--- Assignment 10/10a2.js
+++ Assignment 10/10a2.js
@@ -72,14 +72,38 @@ function getPicsRepresentOfNumber(number) {
     return returnHtml;
 }
 
-// function updateT0Input(t0) {
-//     document.getElementById("updatet0").value = t0;
-// }
+function updateT0Input(t0) {
+    document.getElementById("updatet0").value = t0;
+}
 
-// function updateDInput(difference) {
-//     document.getElementById("updated").value = difference;
-// }
+function updateDInput(difference) {
+    document.getElementById("updated").value = difference;
+}
 
-// function updateNInput(numOfTerms) {
-//     document.getElementById("updaten").value = numOfTerms;
-// }
+function updateNInput(numOfTerms) {
+    document.getElementById("updaten").value = numOfTerms;
+}
+
+var slider1 = document.getElementById("t0");
+var output1 = document.getElementById("demo1");
+output1.innerHTML = slider1.value;
+
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+
+var slider2 = document.getElementById("d");
+var output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
+
+var slider3 = document.getElementById("tn");
+var output3 = document.getElementById("demo3");
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
