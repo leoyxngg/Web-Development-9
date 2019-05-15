3344f9cb8e462ff564084051f67fbecb8ef183fd
apple <apple@Air-Yong-Yang.hitronhub.home>
2019-05-14 17:46:07 -0700

    Deleted all of the uneeded comments

--- Assingment 11/11a1.js
+++ Assingment 11/11a1.js
@@ -23,29 +23,23 @@ function uniKeyCode(event) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/up.png'>"
         msg = new SpeechSynthesisUtterance("jump");
      
-       //settings
-       msg.rate = 1.2; //speech speed - range: 0 to 10
+       msg.rate = 1.2;
        console.log(synth.getVoices())
-       //look into console to see all available voices/languages
        msg.voice = synth.getVoices()[0];
 
-       //speaking trigger
-       synth.cancel(); //cut previous voice short
+       synth.cancel();
        synth.speak(msg);
     }
 
     if(key == 83) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/down.png'>"
         msg = new SpeechSynthesisUtterance("roll");
-     
-       //settings
-       msg.rate = 1.2; //speech speed - range: 0 to 10
+
+       msg.rate = 1.2;
        console.log(synth.getVoices())
-       //look into console to see all available voices/languages
        msg.voice = synth.getVoices()[0];
 
-       //speaking trigger
-       synth.cancel(); //cut previous voice short
+       synth.cancel();
        synth.speak(msg);
     }
 
@@ -53,29 +47,23 @@ function uniKeyCode(event) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/left.png'>"
         msg = new SpeechSynthesisUtterance("roll left");
      
-       //settings
-       msg.rate = 1.2; //speech speed - range: 0 to 10
+       msg.rate = 1.2;
        console.log(synth.getVoices())
-       //look into console to see all available voices/languages
        msg.voice = synth.getVoices()[0];
 
-       //speaking trigger
-       synth.cancel(); //cut previous voice short
+       synth.cancel();
        synth.speak(msg);
     }
 
     if(key == 68) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/right.png'>"
         msg = new SpeechSynthesisUtterance("roll right");
-     
-       //settings
-       msg.rate = 1.2; //speech speed - range: 0 to 10
+    
+       msg.rate = 1.2;
        console.log(synth.getVoices())
-       //look into console to see all available voices/languages
        msg.voice = synth.getVoices()[0];
 
-       //speaking trigger
-       synth.cancel(); //cut previous voice short
+       synth.cancel();
        synth.speak(msg);
     }
 }
