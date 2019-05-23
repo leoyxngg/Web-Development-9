85a1608825bc26a4f105b0a58f1ced1baa866340
Leo Yang <sd02008515@richmw34567bel.rsd38.ca>
2019-05-23 13:43:52 -0700

    Fixed some of the issues in the 11a1.js

--- Assingment 11/11a2.js
+++ Assingment 11/11a2.js
@@ -3,6 +3,7 @@ var synth = window.speechSynthesis;
 var x = 0;
 var keys = [];
 
+
 window.addEventListener("keydown", keydownFunc, false);
 window.addEventListener("keyup", keyupFunc, false);
 
@@ -11,7 +12,6 @@ function keyupFunc(e) {
 }
 
 function keydownFunc(e) {
-
     var e = e || event;
     keys[e.keyCode] = true;
     //alert(e.keyCode);   
@@ -24,7 +24,6 @@ function keydownFunc(e) {
         // do that
         uniKeyCodeEnglish(event);
     }
-
 }
 
 function uniKeyCodeEnglish(event) {
@@ -34,81 +33,46 @@ function uniKeyCodeEnglish(event) {
     if (keys[90] && keys[88]) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/upLeft.png'>";
         msg = new SpeechSynthesisUtterance("Dash Left");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[0];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakEnglish()
     }
 
     // Key M and N combo
-    if (keys[77] && keys[78]) {
+    else if (keys[77] && keys[78]) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/upRight.png'>";
         msg = new SpeechSynthesisUtterance("Dash Right");
 
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[0];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakEnglish()
     }
 
-    if (keyE == 32 || keyE == 13) {
+    else if (keyE == 32 || keyE == 13) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/fire.jpg'>";
         msg = new SpeechSynthesisUtterance("fire");
-
-        //settings
-        msg.rate = 1.2; //speech speed - range: 0 to 10
-        //look into console to see all available voices/languages
-        msg.voice = synth.getVoices()[0];
-        //speaking trigger
-        synth.cancel(); //cut previous voice short
-        synth.speak(msg);
+        speakEnglish()
     }
 
-    if (keys[87] || keys[38]) {
+    else if (keys[87] || keys[38]) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/up.png'>";
         msg = new SpeechSynthesisUtterance("jump");
 
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[0];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakEnglish()
     }
 
-    if (keyE == 83 || keyE == 40) {
+    else if (keyE == 83 || keyE == 40) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/down.png'>";
         msg = new SpeechSynthesisUtterance("roll");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[0];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakEnglish()
     }
 
-    if (keys[65] || keys[37]) {
+    else if (keys[65] || keys[37]) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/left.png'>";
         msg = new SpeechSynthesisUtterance("move left");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[0];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakEnglish()
     }
 
-    if (keyE == 68 || keyE == 39) {
+    else if (keyE == 68 || keyE == 39) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/right.png'>"
         msg = new SpeechSynthesisUtterance("move right");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[0];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakEnglish()
     }
 }
 
@@ -119,81 +83,59 @@ function uniKeyCodeChinese() {
     if (keys[90] && keys[88]) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/upLeft.png'>";
         msg = new SpeechSynthesisUtterance("冲向左边");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[19];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakChinese()
     }
 
     // Key M and N combo
-    if (keys[77] && keys[78]) {
+    else if (keys[77] && keys[78]) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/upRight.png'>";
         msg = new SpeechSynthesisUtterance("冲向右边");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[19];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakChinese()
     }
 
-    if (keyC == 32 || keyC == 13) {
+    else if (keyC == 32 || keyC == 13) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/fire.jpg'>";
         msg = new SpeechSynthesisUtterance("火攻");
-
-        //settings
-        msg.rate = 1.2; //speech speed - range: 0 to 10
-        //look into console to see all available voices/languages
-        msg.voice = synth.getVoices()[19];
-        console.log(synth.getVoices())
-        //speaking trigger
-        synth.cancel(); //cut previous voice short
-        synth.speak(msg);
+        speakChinese()
     }
 
-    if (keyC == 87 || keyC == 38) {
+    else if (keyC == 87 || keyC == 38) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/up.png'>";
         msg = new SpeechSynthesisUtterance("跳");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[19];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakChinese()
     }
 
-    if (keyC == 83 || keyC == 40) {
+    else if (keyC == 83 || keyC == 40) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/down.png'>";
         msg = new SpeechSynthesisUtterance("翻滚");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[19];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakChinese()
     }
 
-    if (keyC == 65 || keyC == 37) {
+    else if (keyC == 65 || keyC == 37) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/left.png'>";
         msg = new SpeechSynthesisUtterance("向左");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[19];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakChinese()
     }
 
-    if (keyC == 68 || keyC == 39) {
+    else if (keyC == 68 || keyC == 39) {
         document.getElementById("actionCenter").innerHTML = "<img src='pics/right.png'>"
         msg = new SpeechSynthesisUtterance("向右");
-
-        msg.rate = 1.2;
-        msg.voice = synth.getVoices()[19];
-
-        synth.cancel();
-        synth.speak(msg);
+        speakChinese()
     }
+}
+
+function speakEnglish() {
+    msg.rate = 1.2;
+    msg.voice = synth.getVoices()[0];
+
+    synth.cancel();
+    synth.speak(msg);
+}
+
+function speakChinese() {
+    msg.rate = 1.2;
+    msg.voice = synth.getVoices()[63]; // would be 19 if it is on my computer
+
+    synth.cancel();
+    synth.speak(msg);
 }
