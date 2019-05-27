d0b843c6d5cd9f8094d37f446b2f3a999994ae5c
apple <apple@air-yong-yang.rsd38.ca>
2019-05-27 13:56:23 -0700

    Added the concept of the Javascript Array worksheet

---
+++ Assingment 11/11b1 - ws.js
@@ -0,0 +1,11 @@
+// var arFruits = [];
+
+// var arFruits = ["apple", "banana", "cherry"];
+// arFruits = [];
+
+// var arFruits = ["apple", "banana", "cherry"]
+// arFruits.splice(1, 0, "avocado")
+
+var arFruits = ["apple", "banana", "cherry"];
+var arFruitsLen = arFruits.length;
+console.log(arFruitsLen);
