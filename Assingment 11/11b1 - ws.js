7214ef6e3b35c464dda30be598a0eff5acbf2270
Leo Yang <sd02008515@richmw34567bel.rsd38.ca>
2019-05-31 14:47:16 -0700

    Added some javascript worksheet questions

--- Assingment 11/11b1 - ws.js
+++ Assingment 11/11b1 - ws.js
@@ -6,6 +6,57 @@
 // var arFruits = ["apple", "banana", "cherry"]
 // arFruits.splice(1, 0, "avocado")
 
-var arFruits = ["apple", "banana", "cherry"];
-var arFruitsLen = arFruits.length;
-console.log(arFruitsLen);
+// var arFruits = ["apple", "banana", "cherry"];
+// var arFruitsLen = arFruits.length;
+
+// var arFruits = ["apple", "banana", "cherry"];
+// arFruits.splice(3, 0, "date");
+
+// var arFruits = ["apple", "banana", "cherry"];
+// arFruits.splice(0, 1);
+
+// var arFruits = ["apple", "banana", "cherry"];
+// arFruits.splice(2, 1);
+
+
+
+
+
+// var arFruits = ["apple", "banana", "cherry"];
+// arFruits.splice(1, 2);
+
+// var arFruits = ["apple", "banana", "cherry"];
+// arFruits.splice(1, 0, "blueberry", "cantaloupe");
+
+// var arFruits = ["apple", "banana", "cherry"];
+// for (var i = 0; i < arFruits.length; i++) { 
+//     console.log(arFruits[i]);
+// }
+
+// var arFruits = ["apple", "banana", "cherry"];
+// var arFruits2 = arFruits.slice();
+
+// var sGreeting = "how are you today?";
+// var arGreeting = [];
+// arGreeting = sGreeting.split(" ");
+
+// var rnd1 = Math.random();
+// var rnd2 = rnd1 * 6;
+// var rnd3 = Math.floor(rnd2);
+
+var arr1 = ["A","B","C","E","F","G"];
+shuffle(arr1)
+function shuffle(pass) { 
+    for (var i = 0; i < pass.length; i++) { 
+        var item = pass[i];
+        var randomitem = pass[Math.floor(Math.random() * pass.length)];
+        pass[i] = randomitem;
+        randomitem = item;
+    }
+}
+console.log(arr1);
+
+// var arr1 = ["apple", "banana", "cherry"];
+// var arr2 = ["apple", "banana", "cherry"];
+// var arr3 = arr1.toString();
+
