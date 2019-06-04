fd65cbacdce3a55bea3d3ac1d1228a945e300753
Leo Yang <sd02008515@richmw34567bel.rsd38.ca>
2019-06-04 14:27:45 -0700

    Finished the Javascript worksheet

--- Assingment 11/11b1 - ws.js
+++ Assingment 11/11b1 - ws.js
@@ -18,9 +18,9 @@
 // var arFruits = ["apple", "banana", "cherry"];
 // arFruits.splice(2, 1);
 
-
-
-
+// var arFruits = ["apple", "banana", "cherry", "date"];
+// arFruits.splice(length-1, 1, "dragonfruit");
+// console.log(arFruits);
 
 // var arFruits = ["apple", "banana", "cherry"];
 // arFruits.splice(1, 2);
@@ -44,17 +44,26 @@
 // var rnd2 = rnd1 * 6;
 // var rnd3 = Math.floor(rnd2);
 
-var arr1 = ["A","B","C","E","F","G"];
-shuffle(arr1)
-function shuffle(pass) { 
-    for (var i = 0; i < pass.length; i++) { 
-        var item = pass[i];
-        var randomitem = pass[Math.floor(Math.random() * pass.length)];
-        pass[i] = randomitem;
-        randomitem = item;
-    }
-}
-console.log(arr1);
+// var arr = ["A", "B", "C", "D", "E", "F"];
+// var times = 0;
+// function shuffle(theArr) {
+//     var random = Math.floor(Math.random() * theArr.length)
+//     var lastItem = theArr[theArr.length-random];
+//     if (lastItem == undefined) {
+//         random = 1;
+//         lastItem = theArr[theArr.length - random];
+//     }
+//     theArr.splice(0, 0, lastItem);
+//     theArr.splice(theArr.length - random, 1);
+//     if (times == 3) {
+//         return;
+//     }
+//     times++;
+//     shuffle(theArr);
+// }
+// console.log(arr);
+// shuffle(arr);
+// console.log(arr);
 
 // var arr1 = ["apple", "banana", "cherry"];
 // var arr2 = ["apple", "banana", "cherry"];
@@ -67,4 +76,3 @@ console.log(arr1);
 // else {
 //     console.log("The two arrays are not the same!")
 // }
-
